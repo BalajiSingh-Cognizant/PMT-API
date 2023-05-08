abbf9f77f08cdb4bef56a1da3a9b79a6ee5f5f6c
Balaji Singh <khbalaji21@gmail.com>
2023-05-08 23:56:40 +0530

    Commit# 2  - Relocated the Manager API to proper location.

--- Services/Manager.API/Program.cs
+++ Services/Manager.API/Program.cs
@@ -1,36 +1,12 @@
 using Manager.API.Data;
 using Manager.API.Repositories.Interfaces;
 using Manager.API.Repositories;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
 using Manager.API.Models;
 using Manager.API.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration;
-
-//var factory = new ConnectionFactory
-//{
-//    HostName = "localhost"
-//};
-
-//var connection = factory.CreateConnection();
-////Here we create channel with session and model
-
-//var channel = connection.CreateModel();
-////declare the queue after mentioning name and a few property related to that
-//channel.QueueDeclare("AssigningTasksQueue", exclusive: false);
-//Set Event object which listen message from chanel which is sent by producer
-//var consumer = new EventingBasicConsumer(channel);
-//consumer.Received += (model, eventArgs) => {
-//    var body = eventArgs.Body.ToArray();
-//    var message = Encoding.UTF8.GetString(body);
-//    Console.WriteLine($"AssigningTasksQueue message received: {message}");
-//};
-////read the message
-//channel.BasicConsume(queue: "AssigningTasksQueue", autoAck: true, consumer: consumer);
 
 builder.Services.Configure<RabbitMqConfiguration>(configuration.GetSection("RabbitMq"));
 builder.Services.AddScoped<ISendAssigningTask, SendAssigningTask>();
